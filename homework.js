1aef737871916f13f7ede2fbb9b4d99a90e60cf8
janet-aharonyan <60620568+janet-aharonyan@users.noreply.github.com>
2020-02-04 01:15:36 +0400

    first homework

---
+++ homework.js
@@ -0,0 +1,130 @@
+// ------1----------
+
+
+let a = 3;
+let b = 14;
+if(b % a === 0) {
+    console.log("1");
+} else {
+    console.log("0");
+}
+
+
+let a = 18;
+let b = 2;
+if(a % b === 1) {
+   console.log("0");
+} else{
+    console.log("1");
+}
+
+let a = 7;
+let b = 21;
+if(b % a === 1) {
+    console.log("0");
+} else {
+    console.log("1");
+ }
+
+
+
+
+
+ // ------2------
+
+ let x = 45;
+ let y = 90;
+ let d = 180;
+ c = x + y ;
+ d -= c ;
+
+
+
+
+ let x = 30;
+ let y = 30;
+c = 180 -( x + y);
+  
+
+
+ let x = 75;
+ let y = 25;
+ let z = 180
+c = 180 - (x + y);
+
+
+
+// -------3--------
+
+let a = 3;
+a = "3" + (3 + 3) + (3 + 3 + 3);
+console.log(a);
+
+let b = 17;
+b = "17" + (17 + 17) + (17 + 17 + 17);
+console.log(b);
+
+let c = 100;
+c = "100" + (100 + 100) + (100 + 100 + 100);
+ console.log(c);
+
+
+
+
+ //------4------
+
+let a = 367;
+if(a % 10 === 1) {
+  console.log(a);
+} else {
+  d = `${a % 10}` + `${Math.floor(a/10)}`;
+  console.log(d);
+}
+
+
+
+let a = 1002;
+if(a % 10 === 1) {
+  console.log(a);
+} else {
+  d = `${a % 10}` + `${Math.floor(a/10)}`;
+  console.log(d);
+}
+
+
+
+
+ let a = 250;
+ if(a % 10 === 0) {
+   console.log(a);
+ } else {
+   d = `${a % 10}` +  `${Math.floor(a/10)}`;
+   console.log(d);
+ } 
+
+
+
+ let a = 8;
+ if(a < 10)
+ console.log|(a);
+ else if(a % 10 === 0) {
+   console.log(a);
+ } else {
+   d = `${a % 10}` +  `${Math.floor(a/10)}`;
+   console.log(d);
+ }  
+
+
+
+
+
+ // ---------5-------
+
+let example = 45 + (-12) + 0 + 3 + (-15)
+answer = example/5;
+alert(answer);
+
+
+let example = 7 + 52 + (-23) + 9 + (-81)
+answer = example/5;
+alert(answer);
